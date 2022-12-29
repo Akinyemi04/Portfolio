c06290bd6d9d4008650f40b7c0a0d7a637721d0b
unknown <gabrielayoku@gmail.com>
2022-12-29 10:09:26 +0100

    fixing minor issue

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -9,12 +9,27 @@ const Contact = () => {
   return (
     <div className='contact-me'>
       <a href="mailto:{gabrielayoku@gmail.com}?subject={subject}&body={content}"><AlternateEmailIcon className='contact-icon'
-      style={{color:'rgb(46, 66, 62)'}}/></a>
-      <a href="tel:{+2348039827644}"><PhoneIphoneIcon className='contact-icon' style={{color:'black'}}/></a>
-      <a href="https://github.com/Akinyemi04?tab=repositories"><GitHubIcon className='contact-icon' style={{color:'black'}}/></a>
-      <a href="sms:{+2348039827644}?body={content}"><MailOutlineIcon className='contact-icon' style={{color:'rgb(204, 185, 173)'}}/></a>
-      <a href="https://twitter.com/alphamhale/"><TwitterIcon className='contact-icon' style={{color:'rgb(9,77,214)'}}/></a>
-      <a href="https://www.instagram.com/alpha_mhalee"><InstagramIcon className='contact-icon instagram'/></a>
+      style={{color:'rgb(46, 66, 62)'}}/>
+      <p>Email</p>
+      </a>
+      <a href="tel:{+2348039827644}"><PhoneIphoneIcon className='contact-icon' style={{color:'black'}}/>
+        <p>Phone number</p>
+      </a>
+      <a href="https://github.com/Akinyemi04?tab=repositories"><GitHubIcon className='contact-icon' style={{color:'black'}}/>
+      <p>Github</p>
+      </a>
+      <a href="sms:{+2348039827644}?body={content}">
+        <MailOutlineIcon className='contact-icon' style={{color:'rgb(204, 185, 173)'}}/>
+        <p>Sms</p>
+      </a>
+      <a href="https://twitter.com/alphamhale/">
+      <TwitterIcon className='contact-icon' style={{color:'rgb(9,77,214)'}}/>
+      <p>Twitter</p>
+      </a>
+      <a href="https://www.instagram.com/alpha_mhalee">
+        <InstagramIcon className='contact-icon instagram'/>
+        <p>Instagram</p>
+        </a>
 
     </div>
   )
