41f3d442a145eac503c37397521433a894c9a5e8
unknown <gabrielayoku@gmail.com>
2023-05-06 00:49:00 +0100

    changing home text animation

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,21 +1,38 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
-import { useDispatch } from 'react-redux'
-import { contact } from './store'
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { contact } from "./store";
 const Home = () => {
-  const Dispatch = useDispatch()
-  function open(){
-    Dispatch(contact.about())
+  const Dispatch = useDispatch();
+  function open() {
+    Dispatch(contact.about());
   }
   return (
-    <div className='home' >
+    <div className="home">
       <main>
         <h1>Akinyemi Gabriel</h1>
-        <p><span>Bringing your digital vision to life, one pixel at a time.</span> </p>
-        <NavLink to='/about' onClick={open} className='about-me'> About me</NavLink>
+        <p>
+          <span>
+            Bringing 
+          </span>
+          <span>your</span>
+          <span>digital</span>
+          <span>vision</span>
+          <span>to</span>
+          <span>life</span>
+          <span>one</span>
+          <span>pixel</span>
+          <span>at</span>
+          <span>a</span>
+          <span>time</span>
+        </p>
+        <NavLink to="/about" onClick={open} className="about-me">
+          {" "}
+          About me
+        </NavLink>
       </main>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
